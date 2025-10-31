06062012189bec46da9507ea927d2919cd07e76f
Anton Kulchytskyi <antek1809@gmail.com>
2025-10-31 21:41:59 +0200

    refactor(database): added migration for is_active field and update all Pydantic schemas accordingly

--- app/models/mixins.py
+++ app/models/mixins.py
@@ -8,10 +8,8 @@ class TimestampMixin:
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
-    updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        server_default=func.now(),
-        onupdate=func.now(),
+    updated_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
 
 
